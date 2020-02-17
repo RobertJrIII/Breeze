c9a35c39c20c62c9dc22f05f6da3f9f6bd5aa858
RobertJrIII <RJ3_the_dev@outlook.com>
2020-02-17 15:32:02 -0800

    added bottom_navbar_menu.xml and fragments

---
+++ app/src/main/java/com/the3rdwheel/breeze/ui/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.the3rdwheel.breeze.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.the3rdwheel.breeze.R
+import com.the3rdwheel.breeze.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        val controller: NavController = findNavController(R.id.nav_host_fragment)
+        val navBar = binding.navBar
+        navBar.setupWithNavController(controller)
+
+        navBar.setOnNavigationItemReselectedListener {
+            Toast.makeText(this, "That Tickles!", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
