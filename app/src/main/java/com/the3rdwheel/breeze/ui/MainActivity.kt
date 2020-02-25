e5b2c70739e2c48f4d070e9d9dfc64d806b6bfff
RobertJrIII <dev.rj3@outlook.com>
2020-02-24 16:56:36 -0800

    updated to android plugin 3.6 using viewBinding

--- app/src/main/java/com/the3rdwheel/breeze/ui/MainActivity.kt
+++ app/src/main/java/com/the3rdwheel/breeze/ui/MainActivity.kt
@@ -2,11 +2,12 @@ package com.the3rdwheel.breeze.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.the3rdwheel.breeze.R
-import kotlinx.android.synthetic.main.activity_main.*
+import com.the3rdwheel.breeze.databinding.ActivityMainBinding.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,17 +15,17 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setContentView(R.layout.activity_main)
 
+        val binding = inflate(layoutInflater)
         val controller: NavController = findNavController(R.id.nav_host_fragment)
-        navBar.apply {
+        binding.navBar.apply {
             setupWithNavController(controller)
         }
-        navBar.setOnNavigationItemReselectedListener {
+        binding.navBar.setOnNavigationItemReselectedListener {
 
         }
-       setSupportActionBar(toolbar)
-
+        setSupportActionBar(binding.toolbar.root as Toolbar)
+        setContentView(binding.root)
 
     }
 
--- app/src/main/java/com/the3rdwheel/breeze/ui/fragments/SettingsFragment.kt
+++ app/src/main/java/com/the3rdwheel/breeze/ui/fragments/SettingsFragment.kt
@@ -1,6 +1,9 @@
 package com.the3rdwheel.breeze.ui.fragments
 
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 
 import androidx.preference.PreferenceFragmentCompat
 import com.the3rdwheel.breeze.R
@@ -12,5 +15,12 @@ class SettingsFragment : PreferenceFragmentCompat() {
         setPreferencesFromResource(R.xml.preferences, rootKey)
     }
 
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
 
 }
