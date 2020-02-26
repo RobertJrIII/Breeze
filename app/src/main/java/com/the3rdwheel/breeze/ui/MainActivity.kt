c2b723cb635a3147f0dc64cb710e2ac88eb669f4
RobertJrIII <dev.rj3@outlook.com>
2020-02-25 17:11:08 -0800

    toolbar displays current fragment name

--- app/src/main/java/com/the3rdwheel/breeze/ui/MainActivity.kt
+++ app/src/main/java/com/the3rdwheel/breeze/ui/MainActivity.kt
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.the3rdwheel.breeze.R
 import com.the3rdwheel.breeze.databinding.ActivityMainBinding
@@ -20,6 +22,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
         val controller: NavController = findNavController(R.id.nav_host_fragment)
         val navBar = binding.navBar
+
         navBar.apply {
             setupWithNavController(controller)
         }
@@ -28,6 +31,17 @@ class MainActivity : AppCompatActivity() {
         }
         setSupportActionBar(binding.mainToolbar.toolbar)
 
+
+        val appBarConfiguration = AppBarConfiguration(
+            topLevelDestinationIds = setOf(
+                R.id.postsFragment,
+                R.id.accountFragment, R.id.inboxFragment,
+                R.id.settingsFragment
+
+            )
+        )
+
+        setupActionBarWithNavController(controller, appBarConfiguration)
 
     }
 
--- app/src/main/java/com/the3rdwheel/breeze/ui/fragments/InboxFragment.kt
+++ app/src/main/java/com/the3rdwheel/breeze/ui/fragments/InboxFragment.kt
@@ -6,16 +6,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.the3rdwheel.breeze.databinding.InboxFragmentBinding
 
-import com.the3rdwheel.breeze.databinding.MessagesFragmentBinding
 
-
-class MessagesFragment : Fragment() {
+class InboxFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val binding = MessagesFragmentBinding.inflate(inflater, container, false)
+        val binding = InboxFragmentBinding.inflate(inflater, container, false)
         return binding.root
     }
 
