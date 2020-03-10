7265edf1533638d9ad895f339a1046c9f03e8f67
RobertJrIII <dev.rj3@outlook.com>
2020-03-10 16:04:19 -0700

    added fastadapter

---
+++ app/src/main/java/com/the3rdwheel/breeze/reddit/adapter/PostItem.kt
@@ -0,0 +1,25 @@
+package com.the3rdwheel.breeze.reddit.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import com.mikepenz.fastadapter.binding.AbstractBindingItem
+import com.the3rdwheel.breeze.R
+import com.the3rdwheel.breeze.databinding.PostItemBinding
+import com.the3rdwheel.breeze.reddit.models.data.children.postdata.PostData
+
+class PostItem(private val data: PostData) : AbstractBindingItem<PostItemBinding>() {
+
+
+    override fun bindView(binding: PostItemBinding, payloads: List<Any>) {
+        binding.postAuthor.text = data.author
+        binding.postTitle.text = data.title
+        binding.postSubbreddit.text = data.subreddit_name_prefixed
+    }
+
+    override fun createBinding(inflater: LayoutInflater, parent: ViewGroup?): PostItemBinding {
+        return PostItemBinding.inflate(inflater, parent, false)
+    }
+
+    override val type: Int
+        get() = R.id.postItem
+}
--- app/src/main/java/com/the3rdwheel/breeze/ui/fragments/PostsFragment.kt
+++ app/src/main/java/com/the3rdwheel/breeze/ui/fragments/PostsFragment.kt
@@ -1,15 +1,17 @@
 package com.the3rdwheel.breeze.ui.fragments
 
-import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import coil.api.load
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.mikepenz.fastadapter.FastAdapter
+import com.mikepenz.fastadapter.adapters.ItemAdapter
 import com.the3rdwheel.breeze.databinding.PostsFragmentBinding
+import com.the3rdwheel.breeze.reddit.adapter.PostItem
+import com.the3rdwheel.breeze.reddit.models.data.children.postdata.PostData
 import com.the3rdwheel.breeze.reddit.retrofit.RedditApi
-import kotlinx.android.synthetic.main.posts_fragment.*
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers.IO
 import kotlinx.coroutines.Dispatchers.Main
@@ -20,7 +22,7 @@ import timber.log.Timber
 
 
 class PostsFragment : Fragment() {
-
+    private lateinit var itemAdapter: ItemAdapter<PostItem>
     private var _binding: PostsFragmentBinding? = null
     private val binding get() = _binding!!
     override fun onCreateView(
@@ -29,8 +31,8 @@ class PostsFragment : Fragment() {
     ): View? {
 
         _binding = PostsFragmentBinding.inflate(inflater, container, false)
-
-
+        itemAdapter = ItemAdapter()
+        binding.postRecyclerview.layoutManager = LinearLayoutManager(context)
         return binding.root
     }
 
@@ -38,6 +40,8 @@ class PostsFragment : Fragment() {
         super.onActivityCreated(savedInstanceState)
 
 
+        val fastAdapter = FastAdapter.with(itemAdapter)
+        binding.postRecyclerview.adapter = fastAdapter
         val redditApi = get<RedditApi>()
 
 
@@ -48,13 +52,8 @@ class PostsFragment : Fragment() {
                     redditApi.getPosts().data.children
 
                 for (post in response) {
-
-                    if (post.data.thumbnail != "self") {
-                        withContext(Main) {
-                            postImageView.load(post.data.thumbnail, get())
-
-                        }
-                        break
+                    withContext(Main) {
+                        itemAdapter.add(PostItem(post.data))
                     }
                 }
 
