3a8b3512122be1e76c6eda08f480345444e0a473
RobertJrIII <dev.rj3@outlook.com>
2020-03-18 12:41:04 -0700

    added themes

--- app/src/main/java/com/the3rdwheel/breeze/ui/fragments/InboxFragment.kt
+++ app/src/main/java/com/the3rdwheel/breeze/ui/fragments/InboxFragment.kt
@@ -7,8 +7,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.the3rdwheel.breeze.databinding.InboxFragmentBinding
-import kotlinx.android.synthetic.main.inbox_fragment.view.*
-import kotlinx.android.synthetic.main.posts_fragment.view.*
 
 
 class InboxFragment : Fragment() {
