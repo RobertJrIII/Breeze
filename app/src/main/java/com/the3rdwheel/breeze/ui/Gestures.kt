978111609f4dadcb2acc4a87e4cc112e806ab8b8
RobertJrIII <dev.rj3@outlook.com>
2020-03-29 19:06:19 -0700

    added the colors to swipe background and they show when swiping

--- app/src/main/java/com/the3rdwheel/breeze/ui/Gestures.kt
+++ app/src/main/java/com/the3rdwheel/breeze/ui/Gestures.kt
@@ -8,14 +8,18 @@ import androidx.recyclerview.widget.ItemTouchHelper.ACTION_STATE_IDLE
 import androidx.recyclerview.widget.RecyclerView
 import com.the3rdwheel.breeze.ui.viewholders.PostViewHolder
 
-const val DOWN_VOTE_COLOR = "#7193FF"
-const val UP_VOTE_COLOR = "#FF4500"
+
+const val DOWN_VOTE_COLOR = "#9494FF"
+const val UP_VOTE_COLOR = "#FF8b60"
+const val SAVE = "#FFFF33"
+const val MORE_OPTIONS = "#C0C0C0"
 
 class Gestures(swipeDirs: Int) : ItemTouchHelper.SimpleCallback(ACTION_STATE_IDLE, swipeDirs) {
 
+//    private var swipeBack = false
 
     override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
-        TODO("Not yet implemented")
+        //TODO add voting, saving, etc. here
     }
 
     override fun onMove(
@@ -46,39 +50,69 @@ class Gestures(swipeDirs: Int) : ItemTouchHelper.SimpleCallback(ACTION_STATE_IDL
         actionState: Int,
         isCurrentlyActive: Boolean
     ) {
+//        recyclerView.setOnTouchListener { v, event ->
+//            swipeBack =
+//                event.action == MotionEvent.ACTION_CANCEL || event.action == MotionEvent.ACTION_UP
+//            false
+//        }
+
         val itemView = viewHolder.itemView
-        val height = itemView.height
         val widthOfSingleLabel = itemView.width / 4
         val width = itemView.width
-        var swipeBackGround: ColorDrawable? = null
 
         if (dX < widthOfSingleLabel && dX > 0) {
-            swipeBackGround = ColorDrawable(Color.parseColor(UP_VOTE_COLOR))
-            swipeBackGround.setBounds(
+
+            setBackground(
+                Color.parseColor(SAVE),
                 itemView.left,
                 itemView.top,
                 dX.toInt(),
-                itemView.bottom
+                itemView.bottom, c
             )
-        } else if (dX > widthOfSingleLabel && dX < width / 2) {
+        } else if (dX > widthOfSingleLabel && ((dX < width / 2) || (dX > width / 2))) {
 
-        } else if (dX < width && dX > widthOfSingleLabel * 3) {
-//            swipeBackGround = ColorDrawable(Color.parseColor(UP_VOTE_COLOR))
-//            swipeBackGround.setBounds(
-//                itemView.left,
-//                itemView.top,
-//                dX.toInt(),
-//                itemView.bottom
-//            )
+            setBackground(
+                Color.parseColor(MORE_OPTIONS),
+                itemView.left,
+                itemView.top,
+                dX.toInt(),
+                itemView.bottom, c
+            )
 
+        } else if (dX < 0 && (width + dX > widthOfSingleLabel * 3)) {
 
-
-
+            setBackground(
+                Color.parseColor(UP_VOTE_COLOR),
+                itemView.right + dX.toInt(),
+                itemView.top,
+                itemView.right,
+                itemView.bottom, c
+            )
         } else {
-
+            setBackground(
+                Color.parseColor(DOWN_VOTE_COLOR),
+                itemView.right + dX.toInt(),
+                itemView.top,
+                itemView.right,
+                itemView.bottom, c
+            )
         }
-        swipeBackGround?.draw(c)
 
         super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive)
+    }
+
+
+    private fun setBackground(
+        color: Int,
+        left: Int,
+        top: Int,
+        right: Int,
+        bottom: Int,
+        canvas: Canvas
+    ) {
+        val background = ColorDrawable(color)
+        background.setBounds(left, top, right, bottom)
+
+        background.draw(canvas)
     }
 }
